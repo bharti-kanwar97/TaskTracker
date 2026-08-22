85b181995ddfd536949785cb18abdc771f046c2f
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-08-22 14:23:48 +0530

    changes in axios

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 // create axios instance
 export const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL:"http://localhost:3000/api/v1/"
+   baseURL: import.meta.env.VITE_API_URL,
+  // baseURL:"http://localhost:3000/api/v1/"
 });
 
 // call axios  request interceptor
