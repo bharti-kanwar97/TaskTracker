b74000bb3fe9943cea84f31fc8a0d1dc2425fb9d
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-09-02 20:10:08 +0530

    Changes in registration controller

--- backend/models/registration.model.js
+++ backend/models/registration.model.js
@@ -8,7 +8,8 @@ const registrationSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        index: true
     },
     password: {
         type: String,
